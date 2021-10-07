248c1bfaf3520c0bcec9e4a6a47ea16756cf4c77
Mudassar Sharif <apple@MacBook-Pro.local>
2021-10-08 04:13:31 +0500

    Fixed calendar, complete color added and other bugs

---
+++ public/runtime.config.js
@@ -0,0 +1,6 @@
+window["runConfig"] = {
+  StagingBaseURL: "https://apex-api.npit.info/api",
+  // LiveBaseURL: "https://api.logicly.ai/api",
+  LiveBaseURL: "http://localhost:56754/api",
+  rcBaseURL: "https://chat1.logicly.ai/"
+};
